f29229fbb0b5399e3125aefbdcabcb149feb9e3f
Erika Foin <erika.foin@gmail.com>
2019-10-02 11:56:45 -0700

    guesses remaining working now

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 import compareResult from './compare-result.js';
 
-//initalize DOM elements//
-//let guessNumber = document.getElementById('number-guess');//
 const submitButton = document.getElementById('submit-button');
 const guessTimes = document.getElementById('guess-times');
 const userResult = document.getElementById('user-result');
 const guess = document.getElementById ('guess');
 
-//add two other things referred to in Index
-
-//set initial state//
 const correctNumber = 7;
 let numGuesses = 4;
 
 submitButton.addEventListener('click', () => {
-    let currentGuess = parseInt(guess.value, 10); //change input to integer//
+    let currentGuess = parseInt(guess.value, 10); 
     numGuesses -= 1;
 
-//messages for user//
-   // let guessTimes = 'You have ' + numGuesses + 'guesses left';//
- //   let guessReply = '';//
-
-//comparing guesses with compareNumbers()//
+    /*if (guessTimes > 0) {
+        guessTimes.textContent = 'You have ' + numGuesses + 'guesses left';
+    }
+    else if (guessTimes < 0) {
+        guessTimes.textContent = 'You are out of guesses!';
+    }*/
 
     if ((compareResult(currentGuess, correctNumber) === 1) && (numGuesses > 0)) {
         userResult.textContent = 'You guessed too high';
@@ -36,35 +32,16 @@ submitButton.addEventListener('click', () => {
     {
         userResult.textContent = 'You are out of guesses. You lose!';
     }
-    //disables button
-    if (guessTimes === 0) {
+
+    if (numGuesses === 0) {
         submitButton.disabled = true;
+        guessTimes.textContent = 'You are out of guesses!';
+
     }
-    else if (guessTimes > 0) {
+    else if (numGuesses > 0) {
         submitButton.disabled = false;
+        guessTimes.textContent = 'You have ' + numGuesses + ' guesses left';
     }
 }
 );
 
-
-// for (let guessTimes = 4; guessTimes < 4; guessTimes--);
-// document.write(guessTimes);
-
-
-/*
-before user clicks:
-random number is generated 
-random numer is stored
-random number will only be regenerated upon page reload (so proceed directly to loop)
-once user clicks:
-(within a loop)
-page takes information
-page converts it into a whole number
-number is stored for comparison
-number is compared with guess
-page returns if the guess was too high, too low, or correct.
-if correct, game ends and victory message is displayed
-if incorrect, failure message is displayed with number of guesses is tallied
-Return to step 1 until one of two things happens - 
-either user runs out of guesses and failure message is displayed
-or victory message is displayed*/
