ef9c074e5351a8b7c5c10ee13cebbc29f1e69626
Erika Foin <erika.foin@gmail.com>
2019-10-02 08:49:53 -0700

    minor edits and text declarations

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 //initalize DOM elements//
 const numberAnswer = document.getElementById('number-answer');
-const userAnswer = document.getElementById('user-answer');
+let numberGuess = document.getElementById('number-guess');
 
 numberAnswer.startingNumber = 0;
 //set initial state//
-let number = 0;
+let guessTimes = 0;
+let userAnswer = userAnswer.enterNumber;
 
 //what events do I need to listen to?
 numberGuess.addEventListener('click', () => {
-    volume = volume + 1;
+    userAnswer.enterNumber = userAnswer;});
 
-    userAnswer.enterNumber = userAnswer;
+for (let guessTimes = 0; guessTimes < 5; guessTimes++);
+document.write(guessTimes);
+
+/*
+before user clicks:
+random number is generated 
+random numer is stored
+random number will only be regenerated upon page reload (so proceed directly to loop)
+once user clicks:
+(within a loop)
+page takes information
+page converts it into a whole number
+number is stored for comparison
+number is compared with guess
+page returns one of two answers
+if yes, game ends and victory message is displayed
+if no, failure message is displayed with number of guesses is tallied
+Return to step 1 until one of two things happens - 
+either user runs out of guesses and failure message is displayed
+or victory message is displayed*/
