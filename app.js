c877643a5c3579b440190bcf9d6c4963b5f7e03a
Erika Foin <erika.foin@gmail.com>
2019-10-03 14:05:55 -0700

    fixed some bugs

--- app.js
+++ app.js
@@ -5,21 +5,12 @@ const guessTimes = document.getElementById('guess-times');
 const userResult = document.getElementById('user-result');
 const guess = document.getElementById ('guess');
 
-//const correctNumber = 7;//
-let correctNumber = Math.floor(Math.random() * Math.floor(20));
-console.log(correctNumber);
+let correctNumber = 7;
 let numGuesses = 4;
 
 submitButton.addEventListener('click', () => {
-    let currentGuess = parseInt(guess.value, 10); 
+    let currentGuess = parseInt(guess.value, 10 + 1); 
     numGuesses -= 1;
-
-    /*if (guessTimes > 0) {
-        guessTimes.textContent = 'You have ' + numGuesses + 'guesses left';
-    }
-    else if (guessTimes < 0) {
-        guessTimes.textContent = 'You are out of guesses!';
-    }*/
 
     if ((compareResult(currentGuess, correctNumber) === 1) && (numGuesses > 0)) {
         userResult.textContent = 'You guessed too high';
@@ -38,12 +29,15 @@ submitButton.addEventListener('click', () => {
     if (numGuesses === 0) {
         submitButton.disabled = true;
         guessTimes.textContent = 'You are out of guesses!';
-
+    }
+    else if (compareResult(currentGuess, correctNumber) === 0) {
+        submitButton.disabled = true;
+        guessTimes.textContent = 'Congratulations!';
     }
     else if (numGuesses > 0) {
         submitButton.disabled = false;
         guessTimes.textContent = 'You have ' + numGuesses + ' guesses left';
     }
+
 }
 );
-
--- compare-result.js
+++ compare-result.js
@@ -8,15 +8,4 @@ const compareResult = (guess, correctNumber) => {
         return (-1);
     }
 };
-
-/*const userResult = () =>
-if (compareResult='0') {
-    ('You got it!');
-} else if (compareResult='1') {
-    ('Too high. Try again.)
-} else if (compareResult='-1') {
-    ('Too low. Try again.)
-};*/
-
 export default compareResult;
-//export default userResult;//
